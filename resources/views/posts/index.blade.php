f43aafabedd6771d24492f1a43b217445ee51d61
Alfan Adi Chandra <46060522+alfancandra@users.noreply.github.com>
2021-12-11 13:59:41 +0700

    menampilkan data

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,8 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    
+    <div class="container">
+        <table class="table table-bordered mt-1">
+            <thead>
+                <tr>
+                    <th scope="col">Title</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Create At</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($posts as $post)
+                <tr>
+                    <td>{{ $post->title }}</td>
+                    <td>{{ $post->status == 0 ? 'Draft':'Publish' }}</td>
+                    <td>{{ $post->created_at->format('d-m-Y') }}</td>
+                    <td class="text-center">
+                         <a href=”” class="btn btn-sm btn-danger">HAPUS</a>
+                    </td>
+                </tr>
+                @empty
+                <tr>
+                    <td class="text-center text-mute" colspan="4">Data post tidak tersedia</td>
+                </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
 </html>
